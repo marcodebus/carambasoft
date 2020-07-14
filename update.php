a8ab53eba92a457a49bfc637c1d0082ecb9dddd4
Marco Debus <33735167+marcodebus@users.noreply.github.com>
2020-07-14 10:25:36 +0200

    Update update.php

--- update.php
+++ update.php
@@ -7,11 +7,7 @@ function execPrint($command) {
     }
 }
 // Print the exec output inside of a pre element
-print("<pre>" . execPrint("git pull https://marcodebus:Marcokite2@github.com/marcodebus/carambasoft.git master") . "</pre>");
-
+print("<pre>" . execPrint("git pull https://marcodebus:Mypasword@github.com/marcodebus/carambasoft.git master") . "</pre>");
 exec("rm -v *");
-echo 'Error with Github Repo https://github.com/marcodebus/carambasoft.git' >index.html
-
-
 //testcode....
 ?>
